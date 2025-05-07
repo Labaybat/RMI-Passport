5fce602541bb4c98231d2b252363dc603c82b551
Labaybat <mending-09.sprout@icloud.com>
2025-05-07 20:10:38 +1200

    Delete supabaseClient.js

--- supabaseClient.js
+++
@@ -1,7 +0,0 @@
-
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
-
-export const supabase = createClient(
-  'https://svkusntallxdxrsgdqzt.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN2a3VzbnRhbGx4ZHhyc2dkcXp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0OTE3NjMsImV4cCI6MjA2MjA2Nzc2M30.-vsOMTTOo8cX5ejbIjwoEGskFx_FZ-yYqVX4EbSDPGQ'
-);
