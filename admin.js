f934c065bdb12fbdcc3a0f09ddec0519310c4a01
Labaybat <mending-09.sprout@icloud.com>
2025-05-08 17:25:15 +1200

    Delete admin.js

--- admin.js
+++
@@ -1,7 +0,0 @@
-import { supabase } from './supabaseClient.js';
-
-const el = document.getElementById('admin-form');
-if (el) el.addEventListener('submit', (e) => {
-  e.preventDefault();
-  alert('admin.html submitted (logic not implemented yet).');
-});
