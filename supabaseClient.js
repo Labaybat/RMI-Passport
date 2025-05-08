aa3275394de7dd7f161d4a06d8b81c06603c0633
Labaybat <mending-09.sprout@icloud.com>
2025-05-08 17:28:58 +1200

    Delete supabaseClient.js

--- supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.42.3/+esm';
-
-export const supabase = createClient(
-  'https://svkusntallxnsdgadzt.supabase.co',
-  'None'
-);
