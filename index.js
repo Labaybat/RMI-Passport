8c1851d0fb5bc60c062f49fc26e4a9332e9710ad
Labaybat <mending-09.sprout@icloud.com>
2025-05-08 17:26:15 +1200

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-import { supabase } from './supabaseClient.js';
-
-const el = document.getElementById('index-form');
-if (el) el.addEventListener('submit', (e) => {
-  e.preventDefault();
-  alert('index.html submitted (logic not implemented yet).');
-});
