571d9d6d86d8229bc9f8e937acdf1927aa95006c
Labaybat <mending-09.sprout@icloud.com>
2025-05-09 18:59:24 +1200

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -2,4 +2,6 @@
 const SUPABASE_URL = 'https://eiuviyizjnfmswfrdigo.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpdXZpeWl6am5mbXN3ZnJkaWdvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY2Nzc1MDIsImV4cCI6MjA2MjI1MzUwMn0.M1E6xOKAc8fsiVkXAxorr1QCRqRedcDv-GNa9CuAE4M';
 
-const supabase = supabase || window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabase = window.supabase?.createClient
+  ? window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
+  : createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
